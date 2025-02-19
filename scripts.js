3e308afe5aca78df1d4e1e85686f9d14f8b65dc0
xpayn3 <36542891+xpayn3@users.noreply.github.com>
2025-02-19 18:58:14 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = today.toLocaleDateString('en-US', options);
+
+document.getElementById('date').innerText = formattedDate;
